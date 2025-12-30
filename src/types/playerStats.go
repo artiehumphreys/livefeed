4c0c7651f6c424a10d5ca7197386f736c010beec
artiehumphreys <artieh2003@gmail.com>
2025-12-29 19:41:51 -0500

    feat(types): go backend for parsing json requests

---
+++ src/types/playerStats.go
@@ -0,0 +1,9 @@
+package types
+
+type PlayerStats struct {
+	ID 	string `json:"id"`
+	Number string `json:"number"`
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
+	Position string `json:"position"`
+}
